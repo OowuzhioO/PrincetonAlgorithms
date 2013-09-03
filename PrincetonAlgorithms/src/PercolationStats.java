cc8a961fc88e1cc00950eba8ecf71a925dc0e0c6
SathishkumarKS <sathish.hari@gmail.com>
2013-09-03 12:25:28 +0530

    Formatted as per checkstyle guidelines

--- PrincetonAlgorithms/src/PercolationStats.java
+++ PrincetonAlgorithms/src/PercolationStats.java
@@ -1,8 +1,26 @@
+/*----------------------------------------------------------------
+ *  Author:        Sathishkumar Kuppuswami
+ *  Written:       02/09/2013
+ *  Last updated:  03/09/2013
+ *
+ *  Implements Percolation Stats API running Monte-carlo simulation on Percolation datatype
+ *
+ *  Compilation:   javac PercolationStats.java
+ *  Execution:     java PercolationStats <<siteLength>> <<SimulationCount>>
+ *  
+ *  % java Percolation <<siteLength>> <<SimulationCount>>
+ *  System percolated after opening xxx sites
+ *----------------------------------------------------------------*/
+
 public class PercolationStats {
 
+	// Length of the site
 	private int siteLength;
+	// # of sites
 	private int siteCount;
+	// # of times to run the simulation
 	private int simulationCount;
+	// List of opened site ratio for each simulation
 	private double[] openedSitesRatio;
 
 	public PercolationStats(int N, int T) {
@@ -10,9 +28,13 @@ public class PercolationStats {
 		simulationCount = T;
 		siteCount = N * N;
 
+		// Run simulation after initialization
 		startSimulation();
 	}
 
+	/**
+	 * Run simulation for specified number of times
+	 */
 	private void startSimulation() {
 		openedSitesRatio = new double[simulationCount];
 
@@ -21,6 +43,9 @@ public class PercolationStats {
 		}
 	}
 
+	/**
+	 * Returns the mean Opened sites ratio across the entire simulation
+	 */
 	public double mean() {
 		double totalOpenedSitesRatio = 0;
 		for (int i = 0; i < simulationCount; i++) {
@@ -29,6 +54,9 @@ public class PercolationStats {
 		return totalOpenedSitesRatio / simulationCount;
 	}
 
+	/**
+	 * Return Standard deviation for the entire simulation
+	 */
 	public double stddev() {
 		double meanValue = mean();
 		double variance = 0;
@@ -41,7 +69,10 @@ public class PercolationStats {
 		return Math.sqrt(variance);
 	}
 
-	public double run() {
+	/**
+	 * Runs a single simulation for the requested site length
+	 */
+	private double run() {
 		double openedSites = 0;
 		Percolation perc = new Percolation(siteLength);
 		while (!perc.percolates()) {
@@ -52,26 +83,41 @@ public class PercolationStats {
 				openedSites++;
 			}
 		}
-		System.out.println("System percolated after opening " + openedSites
-				+ " sites");
 		return openedSites;
 
 	}
 
+	/**
+	 * Returns lower bound of the 95% confidence interval
+	 */
 	public double confidenceLo() {
 		return mean() - (1.96 * (stddev() / Math.sqrt(simulationCount)));
 	}
 
+	/**
+	 * Returns upper bound of the 95% confidence interval
+	 */
 	public double confidenceHi() {
 		return mean() + (1.96 * (stddev() / Math.sqrt(simulationCount)));
 	}
 
 	public static void main(String[] args) {
-		PercolationStats myStats = new PercolationStats(200, 100);
-		System.out.println(myStats.mean());
-		System.out.println(myStats.stddev());
-		System.out.println(myStats.confidenceLo());
-		System.out.println(myStats.confidenceHi());
+		int N = 200;
+		int T = 100;
+		if (args.length == 2) {
+			try {
+				N = Integer.parseInt(args[0]);
+				T = Integer.parseInt(args[1]);
+			} catch (NumberFormatException nfe) {
+				StdOut.println("Invalid command line arguments. Proceeding with application defaults");
+			}
+		}
+
+		PercolationStats myStats = new PercolationStats(N, T);
+		StdOut.println("Mean                    = " + myStats.mean());
+		StdOut.println("Stddev                  = " + myStats.stddev());
+		StdOut.println("95% confidence interval = " + myStats.confidenceLo()
+				+ ", " + myStats.confidenceHi());
 	}
 
 }
