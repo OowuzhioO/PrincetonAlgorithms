b0ec3ee1e43754a4b2db28ce17f1e425717ed187
SathishkumarKS <sathish.hari@gmail.com>
2013-09-03 17:23:33 +0530

    Fix for failing tests and hard tabs

--- PrincetonAlgorithms/src/Percolation.java
+++ PrincetonAlgorithms/src/Percolation.java
@@ -14,181 +14,179 @@
 
 public class Percolation {
 
-	// Array storing Open/Closed state of every site in the system
-	private boolean[] sites;
-	// # of sites
-	private int sitesCount;
-	private WeightedQuickUnionUF weightedQUF;
-	// Reference to index of virtual top site
-	private int virtualTop;
-	// Reference to index of virtual bottom site
-	private int virtualBottom;
-	// Length of the site
-	private int siteLength;
+    // Array storing Open/Closed state of every site in the system
+    private boolean[] sites;
+    // # of sites
+    private int sitesCount;
+    private WeightedQuickUnionUF weightedQUF;
+    // Reference to index of virtual top site
+    private int virtualTop;
+    // Reference to index of virtual bottom site
+    private int virtualBottom;
+    // Length of the site
+    private int siteLength;
 
-	public Percolation(int n) {
-		siteLength = n;
-		sitesCount = n * n;
-		sites = new boolean[sitesCount];
-		// Extra 2 cells to store virtual top and bottom nodes respectively
-		weightedQUF = new WeightedQuickUnionUF(sitesCount + 2);
+    public Percolation(int n) {
+        siteLength = n;
+        sitesCount = n * n;
+        sites = new boolean[sitesCount];
+        // Extra 2 cells to store virtual top and bottom nodes respectively
+        weightedQUF = new WeightedQuickUnionUF(sitesCount + 2);
 
-		// Value of virtual top site (400 in a 20 by 20 system)
-		virtualTop = sitesCount;
-		// Value of virtual top site (401 in a 20 by 20 system)
-		virtualBottom = sitesCount + 1;
-	}
+        // Value of virtual top site (400 in a 20 by 20 system)
+        virtualTop = sitesCount;
+        // Value of virtual top site (401 in a 20 by 20 system)
+        virtualBottom = sitesCount + 1;
+    }
 
-	/**
-	 * Opens up the requested site and connects with the neighbors that are
-	 * already open
-	 * 
-	 * Throws a IndexOutOfBoundsException if given row/column is outside system
-	 * boundary.
-	 */
-	public void open(int row, int column) {
-		isWithinBoundary(row, column);
-		int siteIndex = getIndex(row, column);
-		if (!sites[siteIndex]) {
-			sites[siteIndex] = true;
+    /**
+     * Opens up the requested site and connects with the neighbors that are
+     * already open
+     * 
+     * Throws a IndexOutOfBoundsException if given row/column is outside system
+     * boundary.
+     */
+    public void open(int row, int column) {
+        isWithinBoundary(row, column);
+        int siteIndex = getIndex(row, column);
+        if (!sites[siteIndex]) {
+            sites[siteIndex] = true;
+            StdOut.println("Opened site index " + siteIndex);
 
-			connectWithLeftNeighbor(row, column);
-			connectWithRightNeighbor(row, column);
-			connectWithTopNeighbor(row, column);
-			connectWithBottomNeighbor(row, column);
+            if (siteLength != 1) {
+                connectWithLeftNeighbor(row, column);
+                connectWithRightNeighbor(row, column);
+                connectWithTopNeighbor(row, column);
+                connectWithBottomNeighbor(row, column);
 
-			if (row == 1) {
-				// If the site is a top site,
-				// connect it with virtual top site
-				weightedQUF.union(siteIndex, virtualTop);
-			} else if ((row == siteLength) && isFull(row, column)) {
-				// If the site is a bottom full site,
-				// connect with virtual bottom site
-				weightedQUF.union(siteIndex, virtualBottom);
-			}
+                if (row == 1) {
+                    // If the site is a top site,
+                    // connect it with virtual top site
+                    weightedQUF.union(siteIndex, virtualTop);
+                } else if ((row == siteLength)) {
+                    // If the site is a bottom full site,
+                    // connect with virtual bottom site
+                    weightedQUF.union(siteIndex, virtualBottom);
+                }
+            } else {
+                weightedQUF.union(siteIndex, virtualTop);
+                weightedQUF.union(siteIndex, virtualBottom);
+            }
 
-		}
-	}
+        }
+    }
 
-	/**
-	 * Returns whether the system is percolated or not at any given point in
-	 * time
-	 * 
-	 */
-	public boolean percolates() {
-		return weightedQUF.connected(virtualTop, virtualBottom);
-	}
+    /**
+     * Returns whether the system is percolated or not at any given point in
+     * time
+     * 
+     */
+    public boolean percolates() {
+        return weightedQUF.connected(virtualTop, virtualBottom);
+    }
 
-	/**
-	 * Returns whether the given site is full or not.
-	 * 
-	 * Throws a IndexOutOfBoundsException if given row/column is outside system
-	 * boundary.
-	 */
-	public boolean isFull(int row, int column) {
-		isWithinBoundary(row, column);
-		int siteIndex = getIndex(row, column);
-		return weightedQUF.connected(virtualTop, siteIndex);
-	}
+    /**
+     * Returns whether the given site is full or not.
+     * 
+     * Throws a IndexOutOfBoundsException if given row/column is outside system
+     * boundary.
+     */
+    public boolean isFull(int row, int column) {
+        isWithinBoundary(row, column);
+        int siteIndex = getIndex(row, column);
+        return weightedQUF.connected(virtualTop, siteIndex);
+    }
 
-	/**
-	 * Returns whether the given site is open or closed.
-	 * 
-	 * Throws a IndexOutOfBoundsException if given row/column is outside system
-	 * boundary.
-	 */
-	public boolean isOpen(int row, int column) {
-		isWithinBoundary(row, column);
-		return sites[getIndex(row, column)];
-	}
+    /**
+     * Returns whether the given site is open or closed.
+     * 
+     * Throws a IndexOutOfBoundsException if given row/column is outside system
+     * boundary.
+     */
+    public boolean isOpen(int row, int column) {
+        isWithinBoundary(row, column);
+        return sites[getIndex(row, column)];
+    }
 
-	/**
-	 * Returns whether the given site is full or not.
-	 * 
-	 * Throws a IndexOutOfBoundsException if given row/column is outside system
-	 * boundary.
-	 */
-	private boolean isWithinBoundary(int row, int column) {
-		if (row < 1 || row > siteLength)
-			throw new IndexOutOfBoundsException("row index " + row
-					+ " must be between 1 and " + siteLength);
+    /**
+     * Returns whether the given site is full or not.
+     * 
+     * Throws a IndexOutOfBoundsException if given row/column is outside system
+     * boundary.
+     */
+    private boolean isWithinBoundary(int row, int column) {
+        if (row < 1 || row > siteLength)
+            throw new IndexOutOfBoundsException("row index " + row
+                    + " must be between 1 and " + siteLength);
 
-		if (column < 1 || column > siteLength) {
-			throw new IndexOutOfBoundsException("row index " + column
-					+ " must be between 1 and " + siteLength);
-		}
-		return true;
-	}
+        if (column < 1 || column > siteLength) {
+            throw new IndexOutOfBoundsException("row index " + column
+                    + " must be between 1 and " + siteLength);
+        }
+        return true;
+    }
 
-	/**
-	 * Transforms given row,column pair to an index in single dimensional array
-	 * 
-	 */
-	private int getIndex(int row, int column) {
-		return ((row - 1) * siteLength) + (column - 1);
-	}
+    /**
+     * Transforms given row,column pair to an index in single dimensional array
+     * 
+     */
+    private int getIndex(int row, int column) {
+        return ((row - 1) * siteLength) + (column - 1);
+    }
 
-	/**
-	 * Connects give row/column site with it's neighbor on the left
-	 * 
-	 */
-	private void connectWithLeftNeighbor(int row, int column) {
-		// Check that site is not the first column and left neighbor is open
-		if (column != 1 && isOpen(row, column - 1))
-			weightedQUF.union(getIndex(row, column), getIndex(row, column - 1));
-	}
+    /**
+     * Connects give row/column site with it's neighbor on the left
+     * 
+     */
+    private void connectWithLeftNeighbor(int row, int column) {
+        // Check that site is not the first column and left neighbor is open
+        if (column != 1 && isOpen(row, column - 1))
+            weightedQUF.union(getIndex(row, column), getIndex(row, column - 1));
+    }
 
-	/**
-	 * Returns whether the given site is full or not.
-	 * 
-	 */
-	private void connectWithRightNeighbor(int row, int column) {
-		// Check that site is not the last column and right
-		// neighbor is open
-		if (column != siteLength && isOpen(row, column + 1))
-			weightedQUF.union(getIndex(row, column), getIndex(row, column + 1));
-	}
+    /**
+     * Returns whether the given site is full or not.
+     * 
+     */
+    private void connectWithRightNeighbor(int row, int column) {
+        // Check that site is not the last column and right
+        // neighbor is open
+        if (column != siteLength && isOpen(row, column + 1))
+            weightedQUF.union(getIndex(row, column), getIndex(row, column + 1));
+    }
 
-	/**
-	 * Returns whether the given site is full or not.
-	 * 
-	 */
-	private void connectWithTopNeighbor(int row, int column) {
-		// Check that site is not the first row and top neighbor is open
-		if (row != 1 && isOpen(row - 1, column))
-			weightedQUF.union(getIndex(row, column), getIndex(row - 1, column));
-	}
+    /**
+     * Returns whether the given site is full or not.
+     * 
+     */
+    private void connectWithTopNeighbor(int row, int column) {
+        // Check that site is not the first row and top neighbor is open
+        if (row != 1 && isOpen(row - 1, column))
+            weightedQUF.union(getIndex(row, column), getIndex(row - 1, column));
+    }
 
-	/**
-	 * Returns whether the given site is full or not.
-	 * 
-	 */
-	private void connectWithBottomNeighbor(int row, int column) {
-		// Check that given site is not the last row and bottom neighbor is open
-		if (row != siteLength && isOpen(row + 1, column))
-			weightedQUF.union(getIndex(row, column), getIndex(row + 1, column));
-	}
+    /**
+     * Returns whether the given site is full or not.
+     * 
+     */
+    private void connectWithBottomNeighbor(int row, int column) {
+        // Check that given site is not the last row and bottom neighbor is open
+        if (row != siteLength && isOpen(row + 1, column))
+            weightedQUF.union(getIndex(row, column), getIndex(row + 1, column));
+    }
 
-	public static void main(String[] args) {
-		Percolation _this = null;
-		int N = 200;
-		if (args.length >= 2)
-			throw new RuntimeException(
-					"Command line argument should be a filename or integer");
-		else if (args.length == 1)
-			N = Integer.parseInt(args[0]);
-		_this = new Percolation(N);
-		int openedSites = 0;
-		while (!_this.percolates()) {
-			int i = 1 + StdRandom.uniform(N);
-			int j = 1 + StdRandom.uniform(N);
-			if (!_this.isOpen(i, j)) {
-				_this.open(i, j);
-				openedSites++;
-			}
-		}
-		StdOut.print("System percolated after opening " + openedSites
-				+ " sites");
+    public static void main(String[] args) {
+        In in = new In(args[0]); // input file
+        int N = in.readInt(); // N-by-N percolation system
 
-	}
+        // repeatedly read in sites to open and draw resulting system
+        Percolation perc = new Percolation(N);
+        while (!in.isEmpty()) {
+            int i = in.readInt();
+            int j = in.readInt();
+            perc.open(i, j);
+        }
+        StdOut.println("System percolates " + perc.percolates());
+
+    }
 }
--- PrincetonAlgorithms/src/PercolationStats.java
+++ PrincetonAlgorithms/src/PercolationStats.java
@@ -3,7 +3,7 @@
  *  Written:       02/09/2013
  *  Last updated:  03/09/2013
  *
- *  Implements Percolation Stats API running Monte-carlo simulation on Percolation datatype
+ *  Implements Percolation Stats API running Monte-carlo simulation on Percolation
  *
  *  Compilation:   javac PercolationStats.java
  *  Execution:     java PercolationStats <<siteLength>> <<SimulationCount>>
@@ -14,110 +14,111 @@
 
 public class PercolationStats {
 
-	// Length of the site
-	private int siteLength;
-	// # of sites
-	private int siteCount;
-	// # of times to run the simulation
-	private int simulationCount;
-	// List of opened site ratio for each simulation
-	private double[] openedSitesRatio;
+    // Length of the site
+    private int siteLength;
+    // # of sites
+    private int siteCount;
+    // # of times to run the simulation
+    private int simulationCount;
+    // List of opened site ratio for each simulation
+    private double[] openedSitesRatio;
 
-	public PercolationStats(int N, int T) {
-		siteLength = N;
-		simulationCount = T;
-		siteCount = N * N;
+    public PercolationStats(int N, int T) {
+        siteLength = N;
+        simulationCount = T;
+        siteCount = N * N;
 
-		// Run simulation after initialization
-		startSimulation();
-	}
+        // Run simulation after initialization
+        startSimulation();
+    }
 
-	/**
-	 * Run simulation for specified number of times
-	 */
-	private void startSimulation() {
-		openedSitesRatio = new double[simulationCount];
+    /**
+     * Run simulation for specified number of times
+     */
+    private void startSimulation() {
+        openedSitesRatio = new double[simulationCount];
 
-		for (int i = 0; i < simulationCount; i++) {
-			openedSitesRatio[i] = run() / siteCount;
-		}
-	}
+        for (int i = 0; i < simulationCount; i++) {
+            openedSitesRatio[i] = run() / siteCount;
+        }
+    }
 
-	/**
-	 * Returns the mean Opened sites ratio across the entire simulation
-	 */
-	public double mean() {
-		double totalOpenedSitesRatio = 0;
-		for (int i = 0; i < simulationCount; i++) {
-			totalOpenedSitesRatio += openedSitesRatio[i];
-		}
-		return totalOpenedSitesRatio / simulationCount;
-	}
+    /**
+     * Returns the mean Opened sites ratio across the entire simulation
+     */
+    public double mean() {
+        double totalOpenedSitesRatio = 0;
+        for (int i = 0; i < simulationCount; i++) {
+            totalOpenedSitesRatio += openedSitesRatio[i];
+        }
+        return totalOpenedSitesRatio / simulationCount;
+    }
 
-	/**
-	 * Return Standard deviation for the entire simulation
-	 */
-	public double stddev() {
-		double meanValue = mean();
-		double variance = 0;
-		double meanDifference = 0;
-		for (int i = 0; i < openedSitesRatio.length; i++) {
-			meanDifference += Math.pow(meanValue - openedSitesRatio[i], 2);
-		}
-		variance = meanDifference / simulationCount;
+    /**
+     * Return Standard deviation for the entire simulation
+     */
+    public double stddev() {
+        double meanValue = mean();
+        double variance = 0;
+        double meanDifference = 0;
+        for (int i = 0; i < openedSitesRatio.length; i++) {
+            double currentMeanDiff = meanValue - openedSitesRatio[i];
+            meanDifference += currentMeanDiff * currentMeanDiff;
+        }
+        variance = meanDifference / simulationCount;
 
-		return Math.sqrt(variance);
-	}
+        return Math.sqrt(variance);
+    }
 
-	/**
-	 * Runs a single simulation for the requested site length
-	 */
-	private double run() {
-		double openedSites = 0;
-		Percolation perc = new Percolation(siteLength);
-		while (!perc.percolates()) {
-			int i = 1  + StdRandom.uniform(siteLength);
-			int j = 1 + StdRandom.uniform(siteLength);
-			if (!perc.isOpen(i, j)) {
-				perc.open(i, j);
-				openedSites++;
-			}
-		}
-		return openedSites;
+    /**
+     * Runs a single simulation for the requested site length
+     */
+    private double run() {
+        double openedSites = 0;
+        Percolation perc = new Percolation(siteLength);
+        while (!perc.percolates()) {
+            int i = 1 + StdRandom.uniform(siteLength);
+            int j = 1 + StdRandom.uniform(siteLength);
+            if (!perc.isOpen(i, j)) {
+                perc.open(i, j);
+                openedSites++;
+            }
+        }
+        return openedSites;
 
-	}
+    }
 
-	/**
-	 * Returns lower bound of the 95% confidence interval
-	 */
-	public double confidenceLo() {
-		return mean() - (1.96 * (stddev() / Math.sqrt(simulationCount)));
-	}
+    /**
+     * Returns lower bound of the 95% confidence interval
+     */
+    public double confidenceLo() {
+        return mean() - (1.96 * (stddev() / Math.sqrt(simulationCount)));
+    }
 
-	/**
-	 * Returns upper bound of the 95% confidence interval
-	 */
-	public double confidenceHi() {
-		return mean() + (1.96 * (stddev() / Math.sqrt(simulationCount)));
-	}
+    /**
+     * Returns upper bound of the 95% confidence interval
+     */
+    public double confidenceHi() {
+        return mean() + (1.96 * (stddev() / Math.sqrt(simulationCount)));
+    }
 
-	public static void main(String[] args) {
-		int N = 20;
-		int T = 100;
-		if (args.length == 2) {
-			try {
-				N = Integer.parseInt(args[0]);
-				T = Integer.parseInt(args[1]);
-			} catch (NumberFormatException nfe) {
-				StdOut.println("Invalid command line arguments. Proceeding with application defaults");
-			}
-		}
+    public static void main(String[] args) {
+        int N = 20;
+        int T = 100;
+        if (args.length == 2) {
+            try {
+                N = Integer.parseInt(args[0]);
+                T = Integer.parseInt(args[1]);
+            } catch (NumberFormatException nfe) {
+                StdOut.println("Invalid arguments. Proceeding with defaults");
+            }
+        }
 
-		PercolationStats myStats = new PercolationStats(N, T);
-		StdOut.println("Mean                    = " + myStats.mean());
-		StdOut.println("Stddev                  = " + myStats.stddev());
-		StdOut.println("95% confidence interval = " + myStats.confidenceLo()
-				+ ", " + myStats.confidenceHi());
-	}
+        PercolationStats myStats = new PercolationStats(N, T);
+        StdOut.println("Mean                    = " + myStats.mean());
+        StdOut.println("Stddev                  = " + myStats.stddev());
+        StdOut.println("95% confidence interval = " + myStats.confidenceLo()
+                + ", " + myStats.confidenceHi());
+    }
 
 }
